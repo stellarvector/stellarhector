85ec9af44066402c3916f0566be2f05aef7a35f8
Vik Vanderlinden <vik.vanderlinden@kuleuven.be>
2023-09-04 14:26:23 +0200

    Update error handlers

--- error_handlers/default.py
+++ error_handlers/default.py
@@ -3,4 +3,4 @@ import logging
 async def default(interaction, error):
     logging.getLogger("bot").error(f"Command error: {error}")
 
-    await interaction.response.send_message(f"Sorry, an unknown error occurred, please ask an admin for help.", ephemeral=True)
+    await interaction.response.send_message(content=f"Sorry, an unknown error occurred, please ask an admin for help.")
--- error_handlers/permissions.py
+++ error_handlers/permissions.py
@@ -2,8 +2,8 @@ import discord
 from discord import app_commands
 
 async def check_role_error(interaction: discord.Interaction, error):
-    if isinstance(error, app_commands.errors.MissingRole):
-        await interaction.response.send_message(":no_entry: Don't try, not allowed!", ephemeral=True)
+    if isinstance(error, app_commands.errors.MissingRole) or isinstance(error, app_commands.errors.MissingAnyRole):
+        await interaction.response.send_message(content=":no_entry: Don't try, not allowed!")
         return True
 
     return False
